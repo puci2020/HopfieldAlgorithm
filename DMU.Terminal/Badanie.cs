77cf00fdffca66dffdd76284b87113a3576996e0
puci2020 <adam.pucicki97@gmail.com>
2022-01-13 16:36:37 +0100

    Create random matrix generator

--- DMU.Terminal/Badanie.cs
+++ DMU.Terminal/Badanie.cs
@@ -1,37 +1,35 @@
 ﻿using DMU.Math;
 using System;
 using System.IO;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DMU.Terminal {
   internal class Badanie {
-    internal static void Uruchom() {
-      StreamWriter file = new StreamWriter("Raport1.txt");
+    internal static void Uruchom(bool rand = false, int m = 1) {
 
-      double [,] vectors_I = new double[,] { {-1,-1,-1 }, { -1,-1,1 }, {-1,1,-1}, { -1,1,1}, { 1,-1,-1}, { 1,-1,1}, { 1,1,-1}, { 1,1,1} };
+      StreamWriter file;
+      double[] matrixData = {};
+      int dimension = 3;
 
-      Matrix I = new Matrix(new double[] {
-        0.5,
-        0.5,
-        0.5
-      }, true);
+      if (rand) {
+        file = new StreamWriter("RaportRandom.txt");
+      } else {
+        file = new StreamWriter("Raport1.txt");
+        Console.WriteLine("Podaj elementy macierzy oddzielając je znakiem spacji. Liczba elementów musi być równa {0}", dimension * dimension);
+        string[] array = Console.ReadLine().Split(' ');
+        matrixData = Array.ConvertAll(array, el => double.Parse(el));
+        if (matrixData.Length != dimension * dimension) {
+          Console.WriteLine("Błędnie podana macierz. Uruchom prorgam jeszcze raz");
+          Console.ReadLine();
+        }
+      }
+
       //Console.WriteLine("Podaj wymiar macierzy. Jeśli macierz 3x3 wpisz 3 itd.");
       //int dimension = Convert.ToInt32(Console.ReadLine());
-      int dimension = 3;
-      Console.WriteLine("Podaj elementy macierzy oddzielając je znakiem spacji. Liczba elementów musi być równa {0}", dimension * dimension);
-      string[] array = Console.ReadLine().Split(' ');
+
       //string [] array = {"0", "-1", "-3", "-1", "0", "2", "-3", "2", "0"};
       //string [] array = {"0", "-1", "3", "-1", "0", "2", "3", "2", "0"};
       //string [] array = {"0", "-6", "6", "-6", "0", "4", "6", "4", "0"};
       //string [] array = {"0", "1", "2", "1", "0", "-1", "2", "-1", "0"};
-
-      double[] matrixData = Array.ConvertAll(array, el => double.Parse(el));
-      if (matrixData.Length != dimension * dimension) {
-        Console.WriteLine("Błędnie podana macierz. Uruchom prorgam jeszcze raz");
-        Console.ReadLine();
-      }
-    
 
       double getEnergySync(Matrix matrix, Matrix vn, Matrix vn_1) {
         double energy = 0.0;
@@ -44,84 +42,118 @@ namespace DMU.Terminal {
         return -(energy);
       }
 
-      for (int i = 0; i < vectors_I.GetLength(0); i++) {
-        file.WriteLine("------------------------ Wektor numer {0} ------------------------", i + 1);
-        Console.WriteLine("------------------------ Wektor numer {0} ------------------------", i + 1);
-        Matrix V0 = new Matrix(new double[] {
-          vectors_I[i, 0], vectors_I[i, 1], vectors_I[i, 2]
+      void runHopfieldSync(double[] data) {
+       
+        double [,] vectors_I = new double[,] { {-1,-1,-1 }, { -1,-1,1 }, {-1,1,-1}, { -1,1,1}, { 1,-1,-1}, { 1,-1,1}, { 1,1,-1}, { 1,1,1} };
+
+        Matrix I = new Matrix(new double[] {
+          0.5,
+          0.5,
+          0.5
         }, true);
 
-        Matrix W = new Matrix(matrixData, dimension, dimension);
-        file.WriteLine("Badany wektor:\n {0}", V0.ToString("F1", "\n", " "));
-        Console.WriteLine("Badany wektor:\n {0}", V0.ToString("F1", "\n", " "));
-        file.WriteLine("Badany wektor:\n {0}", V0.ToString("F1", "\n", " "));
-        Console.WriteLine("Macierz wag:\n {0}", W.ToString("F1", "\t", "\n"));
-        file.WriteLine("Macierz wag:\n {0}", W.ToString("F1", "\t", "\n"));
+        for (int i = 0; i < vectors_I.GetLength(0); i++) {
+          file.WriteLine("------------------------ Wektor numer {0} ------------------------", i + 1);
+          Console.WriteLine("------------------------ Wektor numer {0} ------------------------", i + 1);
+          Matrix V0 = new Matrix(new double[] {
+            vectors_I[i, 0], vectors_I[i, 1], vectors_I[i, 2]
+          }, true);
 
-        bool repeat = true;
-        int index = 1;
+          Matrix W = new Matrix(data, dimension, dimension);
+          file.WriteLine("Badany wektor:\n {0}", V0.ToString("F1", "\n", " "));
+          Console.WriteLine("Badany wektor:\n {0}", V0.ToString("F1", "\n", " "));
+          file.WriteLine("Badany wektor:\n {0}", V0.ToString("F1", "\n", " "));
+          Console.WriteLine("Macierz wag:\n {0}", W.ToString("F1", "\t", "\n"));
+          file.WriteLine("Macierz wag:\n {0}", W.ToString("F1", "\t", "\n"));
 
-        double energy_V, energy_V_1;
-        energy_V_1 = 0;
-        Matrix Un;
-        Matrix Vn_1, Vn;
-        Vn_1 = V0;
+          bool repeat = true;
+          int index = 1;
 
-        do {
-          Console.WriteLine("\nKrok {0}", index);
-          file.WriteLine("\nKrok {0}", index);
-          Un = Matrix.Multiply(W, Vn_1);
-          Un = Matrix.Add(Un, I);
-          Console.WriteLine("Potencjał wejściowy:\n{0}", Un.ToString("F1", "\n", " "));
-          file.WriteLine("Potencjał wejściowy:\n{0}", Un.ToString("F1", "\n", " "));
-          Vn = Un.ToBiPolar();
-          Console.WriteLine("Potencjał wyjściowy:\n{0}", Vn.ToString("F1", "\n", " "));
-          file.WriteLine("Potencjał wyjściowy:\n{0}", Vn.ToString("F1", "\n", " "));
-          energy_V = getEnergySync(W, Vn, Vn_1);
+          double energy_V, energy_V_1;
+          energy_V_1 = 0;
+          Matrix Un;
+          Matrix Vn_1, Vn;
+          Vn_1 = V0;
 
-          Console.WriteLine("\nEnergia({0}) = {1}", index, energy_V);
-          file.WriteLine("\nEnergia({0}) = {1}", index, energy_V);
+          do {
+            Console.WriteLine("\nKrok {0}", index);
+            file.WriteLine("\nKrok {0}", index);
+            Un = Matrix.Multiply(W, Vn_1);
+            Un = Matrix.Add(Un, I);
+            Console.WriteLine("Potencjał wejściowy:\n{0}", Un.ToString("F1", "\n", " "));
+            file.WriteLine("Potencjał wejściowy:\n{0}", Un.ToString("F1", "\n", " "));
+            Vn = Un.ToBiPolar();
+            Console.WriteLine("Potencjał wyjściowy:\n{0}", Vn.ToString("F1", "\n", " "));
+            file.WriteLine("Potencjał wyjściowy:\n{0}", Vn.ToString("F1", "\n", " "));
+            energy_V = getEnergySync(W, Vn, Vn_1);
 
-          if (index > 1) {
-            energy_V_1 = getEnergySync(W, Vn, Vn_1);
+            Console.WriteLine("\nEnergia({0}) = {1}", index, energy_V);
+            file.WriteLine("\nEnergia({0}) = {1}", index, energy_V);
 
-            if (!Vn_1.Equals(Vn) && energy_V == energy_V_1) {
-              Console.WriteLine("Wniosek: Punkt [{0}] wpada w cykl [{1}] <-> [{2}]", V0.ToString("F1", "\n", " "), Vn.ToString("F1", "\n", " "), Vn_1.ToString("F1", "\n", " "));
-              file.WriteLine("Wniosek: Punkt [{0}] wpada w cykl [{1}] <-> [{2}]", V0.ToString("F1", "\n", " "), Vn.ToString("F1", "\n", " "), Vn_1.ToString("F1", "\n", " "));
+            if (index > 1) {
+              energy_V_1 = getEnergySync(W, Vn, Vn_1);
+
+              if (!Vn_1.Equals(Vn) && energy_V == energy_V_1) {
+                Console.WriteLine("Wniosek: Punkt [{0}] wpada w cykl [{1}] <-> [{2}]", V0.ToString("F1", "\n", " "), Vn.ToString("F1", "\n", " "), Vn_1.ToString("F1", "\n", " "));
+                file.WriteLine("Wniosek: Punkt [{0}] wpada w cykl [{1}] <-> [{2}]", V0.ToString("F1", "\n", " "), Vn.ToString("F1", "\n", " "), Vn_1.ToString("F1", "\n", " "));
+                repeat = false;
+                Console.WriteLine("\n----- Koniec badania! -----");
+                file.WriteLine("\n----- Koniec badania! -----");
+              }
+              if (Vn_1.Equals(Vn)) {
+                Console.WriteLine("Wniosek: Punkt [{0}] jest zbieżny do punktu [{1}]", V0.ToString("F1", "\n", " "), Vn.ToString("F1", "\n", " "));
+                file.WriteLine("Wniosek: Punkt [{0}] jest zbieżny do punktu [{1}]", V0.ToString("F1", "\n", " "), Vn.ToString("F1", "\n", " "));
+                repeat = false;
+                Console.WriteLine("\n----- Koniec badania! -----");
+                file.WriteLine("\n----- Koniec badania! -----");
+              }
+            } else if (Vn_1.Equals(Vn)) {
+              Console.WriteLine("Wniosek: Punkt [{0}] jest stały!", Vn.ToString("F1", "\n", " "));
+              file.WriteLine("Wniosek: Punkt [{0}] jest stały!", Vn.ToString("F1", "\n", " "));
               repeat = false;
               Console.WriteLine("\n----- Koniec badania! -----");
               file.WriteLine("\n----- Koniec badania! -----");
             }
-            if (Vn_1.Equals(Vn)) {
-              Console.WriteLine("Wniosek: Punkt [{0}] jest zbieżny do punktu [{1}]", V0.ToString("F1", "\n", " "), Vn.ToString("F1", "\n", " "));
-              file.WriteLine("Wniosek: Punkt [{0}] jest zbieżny do punktu [{1}]", V0.ToString("F1", "\n", " "), Vn.ToString("F1", "\n", " "));
+
+            index++;
+
+            if (index == System.Math.Pow(2, dimension) + 1) {
               repeat = false;
               Console.WriteLine("\n----- Koniec badania! -----");
               file.WriteLine("\n----- Koniec badania! -----");
+
             }
-          } else if (Vn_1.Equals(Vn)) {
-            Console.WriteLine("Wniosek: Punkt [{0}] jest stały!", Vn.ToString("F1", "\n", " "));
-            file.WriteLine("Wniosek: Punkt [{0}] jest stały!", Vn.ToString("F1", "\n", " "));
-            repeat = false;
-            Console.WriteLine("\n----- Koniec badania! -----");
-            file.WriteLine("\n----- Koniec badania! -----");
-          }
 
-          index++;
+            Vn_1 = Vn;
 
-          if (index == System.Math.Pow(2, dimension) + 1) {
-            repeat = false;
-            Console.WriteLine("\n----- Koniec badania! -----");
-            file.WriteLine("\n----- Koniec badania! -----");
+          } while (repeat);
 
-          }
-
-          Vn_1 = Vn;
-
-        } while (repeat);
-
+        }
       }
+      if (rand) {
+        Random rnd = new Random();
 
+        double[] randMatrixData = new double[9];
+        for (int i = 0; i < m; i++) {
+          for (int j = 0; j < randMatrixData.Length; j++) {
+            //randMatrixData.SetValue(rnd.Next(-99, 99), j);
+            randMatrixData[j] = rnd.Next(-99, 99);
+
+          }
+
+          Console.WriteLine("-------------------------------------------------------------------");
+          file.WriteLine("-------------------------------------------------------------------");
+          Console.WriteLine("********** Wygenerowana macierz numer {0} **********", i + 1);
+          file.WriteLine("********** Wygenerowana macierz numer {0} **********", i + 1);
+          Console.WriteLine("-------------------------------------------------------------------");
+          file.WriteLine("-------------------------------------------------------------------");
+          runHopfieldSync(randMatrixData);
+
+        }
+
+      } else {
+        runHopfieldSync(matrixData);
+      }
       Console.WriteLine("Wciśnij Enter, aby zapisać wyniki do pliku");
       Console.ReadLine();
       file.Close();
--- DMU.Terminal/Program.cs
+++ DMU.Terminal/Program.cs
@@ -9,9 +9,26 @@ namespace DMU.Terminal
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Wciśnij 1, aby uruchomić algorytm w trybie synchronicznym");
+            Console.WriteLine("Wciśnij 2, aby uruchomić algorytm dla m losowych macierzy 3x3");
+            int mode = Convert.ToInt32(Console.ReadLine());
+
             try
-            {
-                Badanie.Uruchom();
+            {   
+                if (mode == 1)
+                {
+                    Badanie.Uruchom();
+                }
+                if (mode == 2)
+	            {   
+                    Console.WriteLine("Podaj liczbe macierzy do wygenerowania");
+                    int m = Convert.ToInt32(Console.ReadLine());
+                 
+                        Badanie.Uruchom(true , m);
+			
+                    
+
+	            }
             }
             catch (Exception ex)
             {
